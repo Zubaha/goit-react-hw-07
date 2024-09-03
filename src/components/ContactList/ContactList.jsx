2fa100ae822b75304b2aea52f754856c695ebfb0
Zubaha <123296129+Zubaha@users.noreply.github.com>
2024-09-03 15:47:58 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from "react-redux";
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+
+function ContactList() {
+  const contacts = useSelector(selectContacts);
+  const filterValue = useSelector(selectNameFilter);
+
+  const contactsFilter = contacts?.filter((contact) =>
+    contact.name.toLowerCase().includes(filterValue.toLowerCase())
+  );
+
+  return (
+    <ul className={css.list}>
+      {contactsFilter.map((contact) => (
+        <Contact
+          key={contact.id}
+          id={contact.id}
+          name={contact.name}
+          number={contact.number}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ContactList;
